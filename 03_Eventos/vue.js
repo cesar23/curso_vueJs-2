f5319ad040ce344f8f98b1cefe2cea77c3d2de5c
Cesar Auris <solito_203@hotmail.com>
2019-12-01 19:53:14 -0500

    dd

---
+++ 03_Eventos/vue.js
@@ -0,0 +1,27 @@
+new Vue({
+  el: '#eventos',
+  data: {
+    x: 0,
+    y: 0,
+    contador: 0
+  },
+  methods: {
+    mostrarUbicacion: function(evento){
+      this.x = evento.clientX;
+      this.y = evento.clientY;
+    },
+    sumar: function(cantidad){
+      this.contador += cantidad;
+    }
+  }
+})
+
+
+
+
+
+
+
+
+
+
